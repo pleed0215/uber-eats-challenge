d71992d9cc4a72d3f44fff1ad4c4efcea80098f2
이은덕 <pleed0215@ieundeog-ui-MacBookPro.local>
2021-02-11 01:27:06 +0900

    ..

--- src/app.module.ts
+++ src/app.module.ts
@@ -33,7 +33,7 @@ import { UploadsModule } from "./uploads/uploads.module";
                 }),
           }
         : { type: "sqlite", database: "db.sqlite3" }),
-      synchronize: false,
+      synchronize: true,
       logging: process.env.NODE_ENV !== "production",
       entities: [Podcast, Episode, User, Review],
     }),
